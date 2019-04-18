f45686a2a18ddc9c3ef44345f497ca03393400f7
Jiarong Gu <jiaronggu@hotmail.com>
2019-04-18 14:31:15 +1000

    lowercase folders

--- src/Vendora.Web/ClientApp/src/components/Layouts/MainLayout.tsx
+++ src/Vendora.Web/ClientApp/src/components/Layouts/MainLayout.tsx
@@ -3,12 +3,13 @@ import Layout from 'antd/lib/layout';
 import Button from 'antd/lib/button';
 import Breadcrumb from 'antd/lib/Breadcrumb';
 import Menu from 'antd/lib/Menu';
-import { Avatar, Dropdown, Icon, Divider} from 'antd';
+import { Avatar, Dropdown, Icon, Divider } from 'antd';
 import * as styles from './mainLayout.module.less';
+import classnames from 'classnames';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserTie } from '@fortawesome/free-solid-svg-icons'
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import { sinking } from 'redux-sink';
 import { NavigationSink } from '@sinks/navgation';
 
@@ -16,43 +17,62 @@ export interface MainLayoutProps {
   navigation: NavigationSink;
 }
 
+const UserMenu = () => (
+  <div className={styles.dropdownMenu}>
+    <button className={styles.dropdownItem}>Notifications<Icon type={'bell'}></Icon></button>
+    <button className={styles.dropdownItem}>Profile<Icon type={'user'}></Icon></button>
+    <button className={styles.dropdownItem}>Prefrences<Icon type={'setting'}></Icon></button>
+    <hr />
+    <button className={styles.dropdownItem}>Logout&nbsp;<Icon type={'logout'}></Icon></button>
+  </div>
+);
+
 export class MainLayout extends React.PureComponent<MainLayoutProps> {
   render() {
-    const { children, navigation } = this.props;
-    console.info(children);
-    const logoClasses = [styles.logo, styles.menuItem].join(' ');
-    const signInMenuClasses = ['right', styles.menuItem].join(' ');
-    const profileMenyClasses = [styles.menuItem, 'right'].join(' ');
-    const navMenuClass = [styles.menuItem].join(' ');
-    const menu = (
-      <div className={styles.dropdownMenu}>
-        <button className={styles.dropdownItem}>Notifications<Icon type='bell'></Icon></button>
-        <button className={styles.dropdownItem}>Profile<Icon type='user'></Icon></button>
-        <button className={styles.dropdownItem}>Prefrences<Icon type='setting'></Icon></button>
-        <hr />
-        <button className={styles.dropdownItem}>Logout&nbsp;<Icon type='logout'></Icon></button>
-    </div>
-    );
+    const { navigation } = this.props;
+    const routes = navigation.layout.concat().sort((a, b) => b.path.localeCompare(a.path));
+
     return (
       <Layout className={styles.container}>
         <Layout.Header className={styles.header}>
           <Menu theme={'light'} mode={'horizontal'} style={{ lineHeight: '60px' }}>
-            <Menu.Item key={'1'} className={logoClasses}><Link to='/'>LOGO</Link></Menu.Item>
-            <Menu.Item key={'2'} className={signInMenuClasses}><Button className={styles.menuItemButton}>Login/Sign Up</Button></Menu.Item>
-            <Menu.Item key={'3'} className={profileMenyClasses}>
-              <Dropdown overlay={menu} placement="bottomRight">
-                <Avatar className={styles.profileIcon} size={40} icon="user" />
+            <Menu.Item key={'1'} className={classnames(styles.logo, styles.menuItem)}>
+              <Link to={'/'}>LOGO</Link>
+            </Menu.Item>
+
+            <Menu.Item key={'4'} className={styles.menuItem}>
+              <Link to={'/user'}>
+                <FontAwesomeIcon icon={faUserTie} />Talk to Expert
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item key={'5'} className={styles.menuItem}>
+              <Link to={'/user'}>About Us</Link>
+            </Menu.Item>
+
+            <Menu.Item key={'2'} className={classnames('right', styles.menuItem)}>
+              <Button className={styles.menuItemButton}>Login/Sign Up</Button>
+            </Menu.Item>
+
+            <Menu.Item key={'3'} className={classnames('right', styles.menuItem)}>
+              <Dropdown overlay={UserMenu} placement={'bottomRight'}>
+                <Avatar className={styles.profileIcon} size={40} icon={'user'} />
               </Dropdown>
             </Menu.Item>
-            <Menu.Item key={'4'} className={navMenuClass}><Link to='/user'><FontAwesomeIcon icon={faUserTie}/>Talk to Expert</Link></Menu.Item>
-            <Menu.Item key={'5'} className={navMenuClass}><Link to='/user'>About Us</Link></Menu.Item>
           </Menu>
         </Layout.Header>
+
         <Layout.Content>
-          { children ? children : null }
+          <Switch>
+            {routes.map(route =>
+              <Route key={route.path} strict path={route.path} component={route.component} />
+            )}
+          </Switch>
         </Layout.Content>
+
         <Layout.Footer className={styles.footer}>
-        Footer</Layout.Footer>
+          Footer
+        </Layout.Footer>
       </Layout>
     );
   };
--- src/Vendora.Web/ClientApp/src/sinks/index.tsx
+++ src/Vendora.Web/ClientApp/src/sinks/index.tsx
@@ -1,11 +1,13 @@
 import { Home } from '@components/Home';
 import { Quote } from '@components/Quote';
+import { User } from '@components/User';
 
 export const defaultSinkState = {
   navigation: {
     layout: [
       { name: 'Home', path: '/', component: Home },
-      { name: 'Quote', path: '/quote', component: Quote }
+      { name: 'Quote', path: '/quote', component: Quote },
+      { name: 'User', path: '/user', component: User }
     ]
   }
 }
