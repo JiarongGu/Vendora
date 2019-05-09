8c49fb819833dafffb500e573f2d40132e6aaa17
alexia <alexia13.shen@gmail.com>
2019-05-09 10:03:05 +1000

    no message

--- src/Vendora.Web/ClientApp/src/pages/Quote/CreateQuote/CreateQuote.tsx
+++ src/Vendora.Web/ClientApp/src/pages/Quote/CreateQuote/CreateQuote.tsx
@@ -5,7 +5,6 @@ import * as styles from './CreateQuote.module.less';
 import Icon from 'antd/lib/icon';
 import { ContentService } from '@services/common/ContentService';
 import { MainLayoutService } from '@services/laytous/MainLayoutService';
-import Steps from 'antd/lib/steps';
 import Input from 'antd/lib/input';
 import { CurrencyInput } from '@components/Input';
 
@@ -23,18 +22,12 @@ export default class CreateQuote extends React.Component {
   updateAnswer = (value) => {
     this.setState({answer: value})
   };
-
   render() {
     return (
       <div className={styles.mainSection}>
         <div className={styles.container}>
           <div className='row'>
             <div className={styles.stepBarContainer}>
-              <Steps className={styles.stepBar} direction="vertical" current={1}>
-                <Steps.Step title="Finished" description="This is a description." />
-                <Steps.Step title="In Progress" description="This is a description." />
-                <Steps.Step title="Waiting" description="This is a description." />
-              </Steps>
             </div>
 
             <div className={styles.questionGroupContainer}>
@@ -46,6 +39,8 @@ export default class CreateQuote extends React.Component {
                   <CurrencyInput ref={this.inputRef} onChange={this.updateAnswer} />
                   <div>{this.state.answer}</div>
                 </div>
+              </div>
+              <div className={styles.questionContainer}>
                 <div className={styles.question}>
                   <h2>How much deposit do you have?</h2>
                 </div>
