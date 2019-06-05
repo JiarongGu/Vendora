05e2fa64690aec2272ce1a184ba71eda5712cd23
Jiarong Gu <jiaronggu@hotmail.com>
2019-06-05 15:05:29 +0900

    added new form

---
+++ src/Vendora.Database/Helpers/FormHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vendora.Application.Models.Entities;
+
+namespace Vendora.Database.Helpers
+{
+    public static class FormHelper
+    {
+        public static FormMetadata GetBasicEnquiryFormCN() {
+            return new FormMetadata {
+                FormSections = new List<FormSection> {
+                    new FormSection {
+                        Order = 0,
+                        Label = "服务信息"
+                    },
+                    new FormSection {
+                        Order = 1,
+                        Label = "贷款信息"
+                    },
+                    new FormSection {
+                        Order = 2,
+                        Label = "财务信息"
+                    },
+                    new FormSection {
+                        Order = 3,
+                        Label = "个人信息"
+                    }
+                }
+            };
+        }
+
+        public static FormMetadata GetBasicEnquiryFormEN()
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Vendora.Database/Migrations/20190604/AddEnquriyForms.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Vendora.Database.Migrations._20190604
+{
+    [Migration(20190604194801)]
+    public class AddEnquriyForms : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
